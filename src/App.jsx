a8dcd8a9188cec3be79ad174609896bffd37d380
Khurshedspi <khurshed5322@gmail.com>
2024-10-23 11:02:32 +0600

    added all setup for chefs table project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Banner from "./components/banner/Banner"
+import Header from "./components/header/Header"
+import OurRecipes from "./components/OurRecipes"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    {/* header  */}
+    <Header></Header>
+    {/* banner  */}
+    <Banner></Banner>
+    {/* OurRecipes  */}
+    <OurRecipes></OurRecipes>
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="flex flex-col md:flex-row md:justify-between items-center my-8 space-x-4">
+            <h1>header</h1>
+        </div>
+    );
+};
+
+export default Header;
